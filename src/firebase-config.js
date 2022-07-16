29385f4df099f1b290849d7381d598d8e260fab1
unknown <tun.kut@outlook.com>
2022-07-16 20:31:52 +0700

    firebase config

---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+    apiKey: "AIzaSyChSZF7dI3dznxi14PLaLLnKUGkrze3b4c",
+    authDomain: "learn-on-demand-2c06c.firebaseapp.com",
+    projectId: "learn-on-demand-2c06c",
+    storageBucket: "learn-on-demand-2c06c.appspot.com",
+    messagingSenderId: "52929575237",
+    appId: "1:52929575237:web:a91616ef62dadc5534662c",
+    measurementId: "G-1LNRBPV41T"
+  };
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app)
+// const analytics = getAnalytics(app);
