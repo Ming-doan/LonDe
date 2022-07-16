251187c8bc8abdf73b5980790ff8276706683925
Ming-doan <quangminh57dng@gmail.com>
2022-07-16 20:37:49 +0700

    fixbug

--- src/views/screens/UserPage.js
+++ src/views/screens/UserPage.js
@@ -2,13 +2,19 @@ import './scss/Frame.scss'
 import UnitCard from '../components/UnitCard'
 import { BsCoin } from 'react-icons/bs';
 import { GiElectric } from 'react-icons/gi';
+import Spacer from '../components/Spacer';
 
 function UserPage() {
     return (
         <div className="container">
 
             <div className='container-flex'>
+                <div className='userinfo'>
+                    <img src='' className='userinfo-avatar' alt=''></img>
+                    <img src='' className='userinfo-avatar' alt=''></img>
+                </div>
                 <UnitCard icon={<BsCoin></BsCoin>} title={"Coin"} amount={"100"}></UnitCard>
+                <Spacer space={20}></Spacer>
                 <UnitCard icon={<GiElectric></GiElectric>} title={"Energy"} amount={"50"}></UnitCard>
             </div>
 
