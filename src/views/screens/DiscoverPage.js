fd603a07d7ef9d4b9507103b13910f1f635d1328
Ming-doan <quangminh57dng@gmail.com>
2022-07-17 13:31:23 +0700

    final

--- src/views/screens/DiscoverPage.js
+++ src/views/screens/DiscoverPage.js
@@ -1,16 +1,26 @@
+import { useState } from 'react'
 import Button from '../components/Button'
+import Course from '../components/Course'
 import Spacer from '../components/Spacer'
 import './scss/Frame.scss'
 
 function DiscoverPage() {
+    const [show, setSohow] = useState(false)
+
     return (
         <div className="container">
             <div className='container-flex'>
                 <input className='input' placeholder='Type what you want to learn'></input>
-                <Button press={() => { }}>Build</Button>
+                <Button press={() => { setSohow(!show) }}>Build</Button>
             </div>
 
             <Spacer space={20}></Spacer>
+
+            {show ? <div className='video-container'>
+                <Course title={"Flutter"} courses={['1ukSR1GRtMU', 'bKueYVtV0eA', 'FLQ-Vhw1NYQ', 'TSIhiZ5jRB0']}></Course>
+                <Course title={"Flutter"} courses={['duvldqNOcTk', 'iLBYhZcLPrI', 'FLNsVWkeE', 'O_HyaCSL-I8']}></Course>
+
+            </div> : null}
         </div>
     )
 }
